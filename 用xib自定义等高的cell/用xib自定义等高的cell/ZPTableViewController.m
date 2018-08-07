28f716f45adc22dce30aedb840844f4db5d8e20b
赵鹏 <zhaopeng@zhaopengdemacbook-pro.local>
2018-08-07 17:14:17 +0800

    修改批注。

--- 用xib自定义等高的cell/用xib自定义等高的cell/ZPTableViewController.m
+++ 用xib自定义等高的cell/用xib自定义等高的cell/ZPTableViewController.m
@@ -45,7 +45,7 @@
     [super viewDidLoad];
     
     /**
-     如果不在自定义cell类中的相关的方法中撰写加载自定义cell的xib文件的相关代码的话，就要在视图控制器类中的viewDidLoad方法中注册自定义cell的xib文件并且绑定特殊标识符，从而系统会根据注册的xib文件和绑定的特殊标识符来创建新的自定义cell。
+     如果不在自定义cell类中的cellWithTableView方法中撰写加载自定义cell的xib文件的相关代码的话，就要在视图控制器类中的viewDidLoad方法中注册自定义cell的xib文件并且绑定特殊标识符，从而系统会根据注册的xib文件和绑定的特殊标识符来创建新的自定义cell。
      */
 //    UINib *nib = [UINib nibWithNibName:NSStringFromClass([ZPTableViewCell class]) bundle:nil];
 //    [self.tableView registerNib:nib forCellReuseIdentifier:@"deal"];
